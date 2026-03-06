12ccd3f0cdb4f92f6ec63dfa23f9fc6224c8719b
syzham <44723667+syzham@users.noreply.github.com>
2026-03-06 17:50:09 +0800

    Added Unpause support for windows

--- src/core/gameStates/states/pausedState.cpp
+++ src/core/gameStates/states/pausedState.cpp
@@ -25,8 +25,9 @@ void PausedState::OnEnter() {
 }
 
 void PausedState::Update(const float deltaTime) {
-    if (m_stateMachine->GetInputManager().isKeyDown(53)) {
+    if (m_stateMachine->GetInputManager().isKeyDown(53) || m_stateMachine->GetInputManager().isKeyDown(256)) {
         m_stateMachine->GetInputManager().setKeyDown(53, false);
+        m_stateMachine->GetInputManager().setKeyDown(256, false);
         m_popRequested = true;
     }
 
